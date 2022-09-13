e229dcea77a4af70778e98b2f7349d5f7cf56ef2
Josh Thompson <josh.thompson@trinoor.com>
2022-09-13 16:01:02 +1000

    Added ability to login and set sessionStorage to equal the logged in users stats

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -18,6 +18,7 @@ export class LoginComponent implements OnInit {
 
   email = "";
   password = "";
+  x = "";
 
   constructor(private router: Router, private httpClient: HttpClient) { }
 
@@ -33,12 +34,25 @@ export class LoginComponent implements OnInit {
       // If the ok attribute is set to true run:
       if(data.ok){
         alert("Valid Credentials");
-        sessionStorage.setItem('userid', data.userid.toString());
+        // Set user to be logged in
         sessionStorage.setItem('userlogin', data.ok.toString());
+
+        // Set user id
+        sessionStorage.setItem('userid', data.userid.toString());
+
+        // Set username
         sessionStorage.setItem('username', data.username);
-        sessionStorage.setItem('userbirthdate', data.userbirthdate);
-        sessionStorage.setItem('userage', data.userage.toString());
-        
+
+        // Set system role
+        sessionStorage.setItem('system_role', data.system_role);
+
+        // Set groups
+        sessionStorage.setItem('groups', data.groups);
+
+        // View session storage
+        //let x = sessionStorage.getItem('groups');
+        //alert(x);
+
         this.router.navigateByUrl("/account");
       } else {
         alert("Invalid credentials")
