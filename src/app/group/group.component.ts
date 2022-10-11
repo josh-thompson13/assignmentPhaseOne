f393f153858f4f5c2ae90b7d3ab3620216ead151
josh-thompson13 <69380003+josh-thompson13@users.noreply.github.com>
2022-10-11 14:14:47 +1000

    Added ability for logged in user to view the channels associated with the channel

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -10,23 +10,25 @@ export class GroupComponent implements OnInit {
   groupid="";
   groups = JSON.parse(localStorage.getItem('groups') ?? "[]");
   rooms: string[] = [];
-  
+
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    // Get the group id user you have selected
     this.route.paramMap.subscribe(
       params=>{this.groupid = params.get('id') ?? "";}
     )
-    // console.log(this.groups);
-    // console.log(this.groups[1].group_id)
+
+    // For each of the the user has access too, check
+    // the group id of each one for a match
     this.groups.forEach((element: any) => {
       if(element.group_id == this.groupid){
-        this.rooms.push(element.rooms)
+        element.rooms.forEach((elementum: any) => {
+          this.rooms.push(elementum.room_name);
+        });
       }
     });
-    console.log(this.rooms[0][2]);
-   
   }
-
 }
